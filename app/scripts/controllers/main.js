d5689eef1736b59f6641fedc419cd2e9af0efebc
unknown <mdbergt@DNRATWIRM202531.soa.alaska.gov>
2014-08-12 11:03:44 -0800

    initial commit of mostly completed mytodo app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+	.controller('MainCtrl', function($scope, localStorageService){
+		var todosInStore = localStorageService.get('todos');
+		console.log(todosInStore);
+		$scope.todos = todosInStore && todosInStore.split('\n') || [];
+		$scope.$watch('todos', function(){
+			localStorageService.add('todos', $scope.todos.join('\n'));
+		}, true);
+		$scope.addTodo = function(){
+			$scope.todos.push($scope.todo);
+			$scope.todo = '';
+		};
+		$scope.removeTodo = function(index){
+			$scope.todos.splice(index, 1);
+		};
+	});
